e622d99a1ca249c32872c9426e24c33c06ad9974
tenyearssindrome <renk46@mail.ru>
2023-06-22 16:09:19 +0300

    git init

---
+++ api/api/settings.py
@@ -0,0 +1,81 @@
+import environ
+import os
+
+from pathlib import Path
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, './.env'))
+
+SITE_BASE_URL = os.environ.get('SITE_BASE_URL', 'http://localhost')
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'SecretKeyDjango')
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django_filters',
+    'rest_framework',
+    'background_task',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'api.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'api.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
+}
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/static/')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MAX_ATTEMPTS = 1
---
+++ api/api/views.py
@@ -0,0 +1,32 @@
+"""Views"""
+import os
+import json
+import logging
+import pika
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class WebHookGrabber(APIView):
+    """Grabber WebHook"""
+    def get(self, request):
+        logging.info(request.query_params)
+        return Response()
+
+    def post(self, request):
+        logging.info(request.META)
+        logging.info(request.body)
+        logging.info(request.POST)
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters(os.environ.get('RABBIT_HOST', 'localhost'), os.environ.get('RABBIT_PORT', 5672)))
+        channel = connection.channel()
+
+        queue = os.environ.get('RABBIT_QUEUE', 'test_queue')
+
+        channel.queue_declare(queue=queue, durable=True, exclusive=False, auto_delete=False)
+        channel.basic_publish(exchange='', routing_key=queue, body=json.dumps(request.data, indent=4, default=str), properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent))
+
+        channel.close()
+        connection.close()
+
+        return Response()
